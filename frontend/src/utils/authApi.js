8d615c693b23ae70ef498241d33ed02df497dfb1
Gleb Lyashenkov <glepka17@yandex.ru>
2023-02-09 18:06:22 +0300

    fix: изменил base_url

--- frontend/src/utils/authApi.js
+++ frontend/src/utils/authApi.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://auth.nomoreparties.co";
+export const BASE_URL = "https://api.front-mesto.glepka.nomoredomainsclub.ru/";
 
 export const statusCheck = (res) => {
   if (res.ok) {
