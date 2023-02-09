d5430cab342f8f2e82d85ac41bb8a545f6d02a38
Gleb Lyashenkov <glepka17@yandex.ru>
2023-02-09 18:02:07 +0300

    feat: добавил cors

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,30 @@ const { validateLogin, validateRegistration } = require('./middlewares/validatio
 const { PORT = 3000 } = process.env;
 
 const app = express();
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'http://localhost:3001',
+  'http://localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  return next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
